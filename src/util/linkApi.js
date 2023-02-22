329b51f35a94b9bd151f898ec03835674b0ab0ed
Muhammad Safaat <paatlupi01@gmail.com>
2023-02-22 15:29:08 +0700

    update

--- src/util/linkApi.js
+++ src/util/linkApi.js
@@ -1,2 +1,3 @@
 // export const REST_API = "https://backend-buhsar-lilzee.adaptable.app/";
-export const REST_API = "http://localhost:4000/";
+// export const REST_API = "http://localhost:4000/";
+export const REST_API = "https://buhsarback.adaptable.app/";
