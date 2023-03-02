b7a7034517051565f315b2cb3f9b5f99a99d6ce6
Muhammad Safaat <paatlupi01@gmail.com>
2023-03-02 21:51:52 +0700

    up

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express')
 const path = require("path");
 const app = express()
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
 var options = {
     dotfiles: 'ignore',
     etag: false,
