c75da1a0235aba86082513a4c417d2108997ecd3
Muhammad Safaat <paatlupi01@gmail.com>
2023-03-03 07:16:06 +0700

    up

--- src/util/linkApi.js
+++ src/util/linkApi.js
@@ -1,1 +1,2 @@
-export const REST_API = "https://orange-red-panda-veil.cyclic.app/";
+// export const REST_API = "https://orange-red-panda-veil.cyclic.app/";
+export const REST_API = "http://localhost:4000/";
